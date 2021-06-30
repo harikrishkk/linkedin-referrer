cba50d581e1f737ab89c6be17856ae16e3ed86ed
Harikrishnan <harikrishkk@gmail.com>
2021-06-30 15:11:42 -0400

    layout fixes

--- src/wrappers/BannerContainer.js
+++ src/wrappers/BannerContainer.js
@@ -5,9 +5,7 @@ export const BannerContainer = ({ children }) => {
   return (
     <div className="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-12">
       <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
-        <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
-          {children}
-        </div>
+        <div className="flex flex-col sm:text-center sm:mb-0">{children}</div>
       </div>
     </div>
   );
--- src/wrappers/BannerHeader.js
+++ src/wrappers/BannerHeader.js
@@ -5,10 +5,12 @@ import { FaSuperpowers } from 'react-icons/fa';
 /* Design Header for Banner  */
 export const BannerHeader = () => {
   return (
-    <Link to="/" className="mb-6 sm:mx-auto">
-      <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
-        <FaSuperpowers size={28} />
-      </div>
-    </Link>
+    <div className="flex justify-center">
+      <Link to="/" className="mb-6">
+        <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
+          <FaSuperpowers size={28} />
+        </div>
+      </Link>
+    </div>
   );
 };
